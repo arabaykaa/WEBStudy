d18e64510bc8a85374344bbfe00b5cbf250a205c
arabaykaa <56497790+arabaykaa@users.noreply.github.com>
2022-02-04 23:03:17 +0600

    Function empty parameters

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,15 @@
 "use strick";
 
-const mas = [1, 3, 4, 5];
+// function tesArg() {
+//   console.log(arguments.length);
+//   console.log(arguments[0]);
+// }
 
-let a = "Hello";
+// let mas = [1, 2, 3, 4, 5];
 
-console.log(a);
+// tesArg("QWERTY", 123);
+
+let mas1 = [3, 14, 5, 23, 2];
+let mas2 = [31, 12, 5, 3, 0];
+
+console.log(Math.max(...mas1, 22, ...mas2));
