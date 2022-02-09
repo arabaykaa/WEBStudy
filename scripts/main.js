08e2ccc892c601aaa712676418b359c709f8098f
arabaykaa <56497790+arabaykaa@users.noreply.github.com>
2022-02-09 16:34:50 +0600

    Zamykanie js

--- scripts/main.js
+++ scripts/main.js
@@ -9,6 +9,18 @@
 
 // tesArg("QWERTY", 123);
 
-let mas1 = [3, 14, 5, 23, 2];
-let mas2 = [31, 12, 5, 3, 0];
+// let mas1 = [3, 14, 5, 23, 2];
+// let mas2 = [31, 12, 5, 3, 0];
+
+
+//Замыкание
+// function squarM(m) {
+//   return function (num) {
+//     alert(num ** m);
+//   };
+// }
+
+// const addOne = squarM(3);
+
+// addOne(2);
 
