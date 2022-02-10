d0714787400465e87aa8c55ee5b0af616b57bb95
arabaykaa <56497790+arabaykaa@users.noreply.github.com>
2022-02-10 20:24:41 +0600

    Global variables, functions in SASS

--- scripts/main.js
+++ scripts/main.js
@@ -12,7 +12,6 @@
 // let mas1 = [3, 14, 5, 23, 2];
 // let mas2 = [31, 12, 5, 3, 0];
 
-
 //Замыкание
 // function squarM(m) {
 //   return function (num) {
@@ -23,4 +22,17 @@
 // const addOne = squarM(3);
 
 // addOne(2);
+
+//Глобальные объекты 10.02.2022
+// if(!window.Promise){
+//     alert("Old!");
+// }else{
+//     alert("Yong!")
+// }
+
+function count(){
+    alert("Hi");
+
+    count.counter()++;
+}
 
